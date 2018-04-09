b5f4bc4ca7ff1829eb21d44af2946b2b8624e70b
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-04-09 12:52:14 +0530

    changes

--- modules/tools.js
+++ modules/tools.js
@@ -3,7 +3,7 @@ module.exports = {
 		return token.name.call();
 	},
 	tBalance: function(token, addr) {
-		return token.balanceOf.call(addr).toString(10);
+		return (token.balanceOf.call(addr)/1000000000000000000).toString(10);
 	},
 	cAddress: function(contract) {
 		return contract.getContractAddr.call();
