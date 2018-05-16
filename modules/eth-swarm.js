4fc7daa65af0b0fe5cd985cc330ebd62698c815a
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-16 18:37:02 +0530

    chnages for swarm

--- modules/eth-swarm.js
+++ modules/eth-swarm.js
@@ -1,7 +1,11 @@
 module.exports = {
 	uploadFile: function(swarm, filepath, kind, defaultFile) {
-		return swarm.upload({path: filepath, kind: kind, defaultFile: defaultFile});
-	  	// .then(return())
-	  	// .catch(console.log);
+		swarm.upload({path: filepath, kind: kind, defaultFile: defaultFile})
+	  	.then(hash => {
+	  		return hash;
+	  	})
+	  	.catch(error => {
+	  		return error;
+	  	});
 	}
 };
