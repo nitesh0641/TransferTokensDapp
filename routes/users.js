e155c715f56316bbe4064f64a10560ec1343b0b4
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-16 17:58:57 +0530

    chnages for swarm

--- routes/users.js
+++ routes/users.js
@@ -142,10 +142,10 @@ router.post("/uploadFile", function(req, res, next) {
 	  path: filepath,		// path to data / file / directory
 	  kind: "file",			// could also be "file" or "data" or "directory"
 	  defaultFile: ""}) 	// (defaultFile: "/index.html") optional, and only for kind === "directory"
-	  .then(outputHash)
+	  .then(hash)
 	  .catch(console.log);
 
-	res.json({"hash": outputHash});
+	res.json({"hash": hash});
 });
 
 router.post("/uploadJSONData", function(req, res, next) {
