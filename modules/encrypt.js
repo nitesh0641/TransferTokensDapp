b9924ccc50fb0b1326eb1edc04c70678215b9440
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-17 12:59:56 +0530

    changes for swarm

--- modules/encrypt.js
+++ modules/encrypt.js
@@ -1,12 +1,12 @@
 module.exports = {
-    encryptStringWithRsaPublicKey: function(toEncrypt, relativeOrAbsolutePathToPublicKey) {
+    encryptStringWithRsaPublicKey: function(crypto, path, fs, toEncrypt, relativeOrAbsolutePathToPublicKey) {
         var absolutePath = path.resolve(relativeOrAbsolutePathToPublicKey);
         var publicKey = fs.readFileSync(absolutePath, "utf8");
         var buffer = new Buffer(toEncrypt);
         var encrypted = crypto.publicEncrypt(publicKey, buffer);
         return encrypted.toString("base64");
     },
-    decryptStringWithRsaPrivateKey: function(toDecrypt, relativeOrAbsolutePathtoPrivateKey) {
+    decryptStringWithRsaPrivateKey: function(crypto, path, fs, toDecrypt, relativeOrAbsolutePathtoPrivateKey) {
         var absolutePath = path.resolve(relativeOrAbsolutePathtoPrivateKey);
         var privateKey = fs.readFileSync(absolutePath, "utf8");
         var buffer = new Buffer(toDecrypt, "base64");
