54d0f4a5be53d764ecf58b098f5fcc178017c770
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-17 10:39:35 +0530

    chages for swarm

--- routes/eth-swarm.js
+++ routes/eth-swarm.js
@@ -14,10 +14,10 @@ router.post("/uploadFile", function(req, res, next) {
 	  path: filepath,		// path to data / file / directory
 	  kind: "file",			// could also be "file" or "data" or "directory"
 	  defaultFile: ""}) 	// (defaultFile: "/index.html") optional, and only for kind === "directory"
-	  .then(web3Message)
+	  .then(res.json({"hash": web3Message}))
 	  .catch(console.log);
 
-	res.json({"hash": web3Message});
+	// res.json({"hash": web3Message});
 });
 
 router.post("/uploadJSONData", function(req, res, next) {
