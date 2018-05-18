9d691602f586d40378febc049f4b8aff093a135e
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-18 13:06:37 +0530

    changes for swarm

--- routes/eth-swarm.js
+++ routes/eth-swarm.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var Web3 = require('web3');
-// var crypto = require('crypto');
-var ursa = require('ursa');
+var crypto = require('crypto');
 var path = require('path');
 var fs = require('fs');
+var fstream = require('fstream');
+var tar = require('tar');
+// var ursa = require('ursa');
+
 var admin = require('../modules/admin');
 var tokens = require('../modules/tools');
 const swarm = require("swarm-js").at("http://127.0.0.1:8500");
@@ -14,46 +17,77 @@ var web3Message = '';
 
 router.post("/uploadFile", function(req, res, next) {
 	var filepath = req.body.filepath;
-	
-	// user1 has his private and user2's public key
-	var privkeyUser1 = ursa.createPrivateKey(fs.readFileSync('/var/crypto/user1/privkey.pem'));
-	var pubkeyUser2 = ursa.createPublicKey(fs.readFileSync('/var/crypto/user2/pubkey.pem'));
+	var pubkeyUser1 = '/var/crypto/user1/pubkey.pem';
 
-	console.log('Encrypt with User2 Public; Sign with User1 Private');
+	console.log('Encrypt with User1 Public');
 
-	fs.readFile(filepath, 'utf8', function(err, contents) {
-		var buffer = new Buffer(contents);
-		enc = pubkeyUser2.encrypt(buffer, 'utf8', 'base64');
-		sig = privkeyUser1.hashAndSign('sha256', buffer, 'utf8', 'base64');
-		console.log('encrypted', enc, '\n');
-		console.log('signed', sig, '\n');
-		// encryptedFile = encrypt.encryptStringWithRsaPublicKey(crypto, path, fs, contents, pubkey);
-	    var stream = fs.createWriteStream('/var/www/TransferTokensDapp/uploads/encrypted.png');
-		stream.once('open', function(fd) {
-		  stream.write(encryptedFile);
-		  stream.end();
-		});
-	});
+	var read = fstream.Reader(filepath),
+		pack = tar.Pack(),
+		writer = fstream.Writer('./out.tar');
+
+	read.pipe(pack).pipe(writer);
+
+	// fs.readFile(filepath, 'utf8', function(err, contents) {
+	// 	encryptedFile = encrypt.encryptStringWithRsaPublicKey(crypto, path, fs, contents, pubkey);
+	//     var stream = fs.createWriteStream('/var/www/TransferTokensDapp/uploads/encrypted.png');
+	// 	stream.once('open', function(fd) {
+	// 	  stream.write(encryptedFile);
+	// 	  stream.end();
+	// 	});
+	// });
 	
-	swarm.upload('/var/www/TransferTokensDapp/uploads/encrypted.png')
-	  .then(function(hash){
-	  	web3Message = hash;
-	  	res.json({"hash": web3Message});
-	  })
-	  .catch(console.log);
-	
-	// swarm.upload({
-	//   path: encryptedFile,		// path to data / file / directory
-	//   kind: "file",			// could also be "file" or "data" or "directory"
-	//   defaultFile: ""}) 	// (defaultFile: "/index.html") optional, and only for kind === "directory"
+	// swarm.upload('/var/www/TransferTokensDapp/uploads/encrypted.png')
 	//   .then(function(hash){
 	//   	web3Message = hash;
 	//   	res.json({"hash": web3Message});
 	//   })
 	//   .catch(console.log);
 
-	// res.json({"hash": web3Message});
+	res.json({"hash": web3Message});
 });
+
+// -- encryption using ursa
+// router.post("/uploadFile", function(req, res, next) {
+// 	var filepath = req.body.filepath;
+	
+// 	// user1 has his private and user2's public key
+// 	var privkeyUser1 = ursa.createPrivateKey(fs.readFileSync('/var/crypto/user1/privkey.pem'));
+// 	var pubkeyUser2 = ursa.createPublicKey(fs.readFileSync('/var/crypto/user2/pubkey.pem'));
+
+// 	console.log('Encrypt with User2 Public; Sign with User1 Private');
+
+// 	fs.readFile(filepath, 'utf8', function(err, contents) {
+// 		var buffer = new Buffer(contents);
+// 		enc = pubkeyUser2.encrypt(buffer, 'utf8', 'base64');
+// 		sig = privkeyUser1.hashAndSign('sha256', buffer, 'utf8', 'base64');
+// 		console.log('encrypted', enc, '\n');
+// 		console.log('signed', sig, '\n');		
+// 	    var stream = fs.createWriteStream('/var/www/TransferTokensDapp/uploads/encrypted.png');
+// 		stream.once('open', function(fd) {
+// 		  stream.write(encryptedFile);
+// 		  stream.end();
+// 		});
+// 	});
+	
+// 	swarm.upload('/var/www/TransferTokensDapp/uploads/encrypted.png')
+// 	  .then(function(hash){
+// 	  	web3Message = hash;
+// 	  	res.json({"hash": web3Message});
+// 	  })
+// 	  .catch(console.log);
+	
+// 	// swarm.upload({
+// 	//   path: encryptedFile,		// path to data / file / directory
+// 	//   kind: "file",			// could also be "file" or "data" or "directory"
+// 	//   defaultFile: ""}) 	// (defaultFile: "/index.html") optional, and only for kind === "directory"
+// 	//   .then(function(hash){
+// 	//   	web3Message = hash;
+// 	//   	res.json({"hash": web3Message});
+// 	//   })
+// 	//   .catch(console.log);
+
+// 	// res.json({"hash": web3Message});
+// });
 
 router.post("/downloadData", function(req, res, next) {
 
