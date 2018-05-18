a19c12f40ee138d92c046a75060fc8cceb280e85
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-18 16:23:33 +0530

    changes for swarm

--- routes/eth-swarm.js
+++ routes/eth-swarm.js
@@ -26,7 +26,7 @@ router.post("/uploadFile", function(req, res, next) {
 	
 	// extracting a directory
 	// console.log("before cd => "+__dirname);
-	fs.createReadStream('my-tarball.tar').pipe(tar.extract('image.png'));
+	fs.readFile('my-tarball.tar').pipe(tar.extract('image.png'));
 
 	// fs.readFile(filepath, 'utf8', function(err, contents) {
 	// 	encryptedFile = encrypt.encryptStringWithRsaPublicKey(crypto, path, fs, contents, pubkey);
