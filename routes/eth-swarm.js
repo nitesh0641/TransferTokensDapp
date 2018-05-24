5fd52de340421858146e6f1e07de2da708efe4ec
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-24 12:23:56 +0530

    changes for swarm

--- routes/eth-swarm.js
+++ routes/eth-swarm.js
@@ -30,7 +30,9 @@ router.post("/uploadFile", function(req, res, next) {
 	// var IV = new Buffer("nc$1238*6089alch");
 
 	fs.readFile(pubkey, 'utf8', function(err, contents) {
-		pubkey = new Buffer(contents, 'hex');//length=30
+		pubkey = new Buffer(contents, 'base64');
+		pubkey = pubkey.toString('binary', 0, 16);
+		//length=16
 		console.log(pubkey);
 	});
 	
