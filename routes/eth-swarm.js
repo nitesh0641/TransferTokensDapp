266751c6666967a74860401d4e12940eefe5d2ae
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-24 12:33:02 +0530

    changes for swarm

--- routes/eth-swarm.js
+++ routes/eth-swarm.js
@@ -37,7 +37,7 @@ router.post("/uploadFile", function(req, res, next) {
 	});
 	
 	var read = fstream.Reader(filepath);
-	var	ency = crypto.createCipheriv('aes-256-ctr', pubkey, IV);
+	var	ency = crypto.createCipheriv('aes-256-cbc', pubkey, IV);
 	// var	ency = crypto.createCipher('aes-128-ccm', pubkey, IV);
 	var	writer = fstream.Writer(protected+filename+".enc");
 	read.pipe(ency).pipe(writer);
