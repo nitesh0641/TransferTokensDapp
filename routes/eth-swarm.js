3b038d122a1cc5f01582fff897a16ac1a905d28d
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-29 15:26:01 +0530

    changes for swarm

--- routes/eth-swarm.js
+++ routes/eth-swarm.js
@@ -113,11 +113,11 @@ router.post("/downloadData", function(req, res, next) {
 			var IV = new Buffer(req.body.password, 'hex');
 			var cipher_blob = IV.toString().split("$");
 			if(cipher_blob[0] == 'nc'){
-				fs.readFile(downloadFile, function(err, contents) {
+				fs.readFile(downloadFile, 'utf8', function(err, contents) {
 					fs.readFile(pubkey, 'utf8', function(err, key) {
 						var	dency = crypto.createDecipheriv('aes-256-cbc', key.substring(0,32), IV),
-							decoded = dency.update(contents, 'utf8', 'base64');
-							decoded += dency.final('base64');
+							decoded = dency.update(contents, 'base64', 'utf8');
+							decoded += dency.final('utf8');
 						var filedata = new Buffer(decoded, 'base64');
 						filedata = filedata.toString('ascii');
 						// var	writer = fstream.Writer(downloadFile);
