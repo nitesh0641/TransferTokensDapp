49552918b0e01318697f69b1bd1e03cbf1797718
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-05-31 12:25:47 +0530

    changes for swarm

--- routes/eth-swarm.js
+++ routes/eth-swarm.js
@@ -79,7 +79,7 @@ router.post("/uploadFile", function(req, res, next) {
 	fs.readFile(pubkey, 'utf8', function(err, contents) {
 		pdfParser.on("pdfParser_dataError", errData => console.error(errData.parserError) );
 	    pdfParser.on("pdfParser_dataReady", pdfData => {
-	        // fs.writeFile("./pdf2json/test/F1040EZ.json", JSON.stringify(pdfData));
+	        fs.writeFile("./F1040EZ.json", JSON.stringify(pdfData));
 	        var fileBuff = new Buffer(JSON.stringify(pdfData));
 	        var	ency = crypto.createCipheriv('aes-256-cbc', contents.substring(0,32), IV);
 			var encryptdata = ency.update(fileBuff, 'utf8', 'hex');
