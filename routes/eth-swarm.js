40f5171b48dcad559148e164b04751bf3fdf7a69
nlp_nitesh <nitesh.chaudhry@nlpcaptcha.com>
2018-06-04 11:54:42 +0530

    changes for swarm

--- routes/eth-swarm.js
+++ routes/eth-swarm.js
@@ -232,7 +232,7 @@ router.post("/isAvailable", function(req, res, next) {
 	request(url, function(error, response, body) {
 		if (!error && response.statusCode == 200) {
 			var rawData = JSON.parse(body);
-			if(!rawData.entries && rawData.entries[0].hash == filehash){
+			if(!rawData.entries){
 				res.json({
 					"status":"200 OK",
 					"hash": filehash,
